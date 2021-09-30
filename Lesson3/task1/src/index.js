ff6df201736617e9b67aeb24d07b8942d1fc1ea8
Antikiok <antikiok@gmail.com>
2021-09-30 09:56:23 +0300

    1

--- Lesson3/task1/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import SearchField from 'search.jsx';
-import './index.scss';
-import './search.scss';
-
-ReactDOM.render(<SearchField name="Tom" />, document.getElementById('root'));

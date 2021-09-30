63ead496fa3390b6f7434e8735c580ef517568b2
Antikiok <antikiok@gmail.com>
2021-09-30 15:06:51 +0300

    1

---
+++ Lesson3/task2/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import { Search } from 'Search.jsx';
+import './index.scss';
+import './search.scss';
+
+ReactDOM.render(<Search name="Tom" />, document.getElementById('root'));

0aa31b0fbe5245fc81fac1e3cabceabbd853f97e
Antikiok <antikiok@gmail.com>
2021-10-01 14:42:11 +0300

    1

--- Lesson3/task3/src/UserInfo.jsx
+++ Lesson3/task3/src/UserInfo.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import Avatar from './Avatar.jsx';
 
 const UserInfo = props => {
-  console.log(props);
   return (
     <div className="user-info">
-      <Avatar user={props.user} />
+      <Avatar avatarUrl={props.user.avatarUrl} name={props.user.name} />
       <div className="user-info__name">{props.user.name}</div>
     </div>
   );

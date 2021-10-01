d54236d188e204425abdcf7f0b04465836a80a02
Antikiok <antikiok@gmail.com>
2021-10-01 18:19:00 +0300

    1

--- Lesson3/task5/src/App.jsx
+++ Lesson3/task5/src/App.jsx
@@ -1,8 +1,14 @@
-// import React from 'react';
-// import Profile from './Profile.jsx';
+import React from 'react';
+import moment from 'moment';
+import Profile from './Profile.jsx';
 
-// const App = () => (
+const App = () => (
+  <Profile
+    firstName={'John'}
+    lastName={'Doe'}
+    birthDate={moment(new Date(1991, 0, 17)).format('DD MMM YY')}
+    birthPlace={'London'}
+  />
+);
 
-// );
-
-// export default App;
+export default App;
--- Lesson3/task5/src/index.jsx
+++ Lesson3/task5/src/index.jsx
@@ -1,15 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import moment from 'moment';
+import App from './App.jsx';
 
-import Profile from './Profile.jsx';
-
-ReactDOM.render(
-  <Profile
-    firstName={'John'}
-    lastName={'Doe'}
-    birthDate={moment(new Date(1991, 0, 17)).format('DD MMM YY')}
-    birthPlace={'London'}
-  />,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));

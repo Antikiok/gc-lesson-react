143673abd34a5bd81ad24162d966b4095931d5af
Antikiok <antikiok@gmail.com>
2021-10-01 23:52:23 +0300

    1

---
+++ Lesson4/task1/src/Counter.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './counter.scss';
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+    setInterval(() => {
+      this.setState(
+        {
+          counter: this.state.counter + 1,
+        },
+        1000,
+      );
+    });
+  }
+
+  render() {
+    console.log(this.state.counter);
+
+    return <div className="counter">{this.state.counter}</div>;
+  }
+}
+export default Counter;

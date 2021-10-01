2ed72e9a485f9f8f327b424579cf3b583a4535cb
Antikiok <antikiok@gmail.com>
2021-10-02 00:22:47 +0300

    1

--- Lesson4/task1/src/App.jsx
+++ Lesson4/task1/src/App.jsx
@@ -3,9 +3,9 @@ import Counter from './Counter.jsx';
 
 const App = () => (
   <>
-    <Counter start={5} />
-    <Counter start={12} />
-    <Counter start={-21} />
+    <Counter start={5} interval={1000} />
+    <Counter start={12} interval={2000} />
+    <Counter start={-21} interval={3000} />
   </>
 );
 

9d751bb3fb5c2726db642fa2122995d996683e02
Antikiok <antikiok@gmail.com>
2021-10-02 23:34:16 +0300

    1

--- Lesson5/task2/src/App.jsx
+++ Lesson5/task2/src/App.jsx
@@ -1,17 +1,6 @@
 import React from 'react';
-import Clock from './Clock.jsx';
-import './clock.scss';
-import './index.scss';
+import GoodButton from './GoodButton.jsx';
 
-const App = () => {
-  const app = (
-    <>
-      <Clock location={'London'} offset={0} />
-      <Clock location={'Kyiv'} offset={2} />
-      <Clock location={'New York'} offset={-5} />
-    </>
-  );
-  return app;
-};
+const App = () => <GoodButton />;
 
 export default App;

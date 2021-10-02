07eaa4bf4cfff2b8deed97b044e33a8965cf4556
Antikiok <antikiok@gmail.com>
2021-10-02 23:18:15 +0300

    1

---
+++ Lesson5/task3/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoodButton from './GoodButton.jsx';
+
+const App = () => {
+  const app = (
+    <>
+      <GoodButton />
+    </>
+  );
+  return app;
+};
+
+export default App;

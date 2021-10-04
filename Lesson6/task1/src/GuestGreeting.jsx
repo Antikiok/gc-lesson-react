25c50dd349505bc388d1c5f27df6589e87775dbb
Antikiok <antikiok@gmail.com>
2021-10-05 00:05:54 +0300

    1

--- Lesson6/task1/src/GuestGreeting.jsx
+++ Lesson6/task1/src/GuestGreeting.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const GuestGreeting = () => <h1 className="logout">{'Hi stranger. Please login 🚀'}</h1>;
+const GuestGreeting = () => <h1>{'Hi stranger. Please login 🚀'}</h1>;
 
 export default GuestGreeting;

afc901035cb75b7d3ec297333c8eb62a6beb4eb2
Antikiok <antikiok@gmail.com>
2021-10-07 18:40:38 +0300

    1

--- Lesson8/task3/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Page from './Page.jsx';
-
-const App = () => <Page />;
-
-export default App;
--- Lesson8/task3/src/index.jsx
+++ Lesson8/task3/src/index.jsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import './index.scss';
-import App from './App.jsx';
+import Dimensions from './Dimensions.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Dimensions />, document.getElementById('root'));

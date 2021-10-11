b5b8c4095e0c29f746f2355a0c596815b28b4157
Antikiok <antikiok@gmail.com>
2021-10-11 16:03:09 +0300

    1

--- Lesson9/task2/src/App.jsx
+++ Lesson9/task2/src/App.jsx
@@ -4,29 +4,12 @@ import UserForm from './UserForm.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      visible: true,
-    };
   }
 
-  toggle = () => {
-    this.setState({
-      visible: !this.state.visible,
-    });
-  };
-
   render() {
-    const localTime = this.props.localTimeZone;
     return (
       <>
-        <button className="btn" onClick={this.toggle}>
-          Hide
-        </button>
-        {this.state.visible &&
-          localTime.map(time => (
-            <UserForm key={time.id} location={time.location} offset={time.offset} />
-          ))}
+        <UserForm />
       </>
     );
   }
--- Lesson9/task2/src/index.jsx
+++ Lesson9/task2/src/index.jsx
@@ -4,22 +4,4 @@ import React from 'react';
 import './index.scss';
 import App from './App.jsx';
 
-const localTimeZone = [
-  {
-    id: 'id-0',
-    location: 'London',
-    offset: 0,
-  },
-  {
-    id: 'id-1',
-    location: 'Kyiv',
-    offset: 2,
-  },
-  {
-    id: 'id-2',
-    location: 'New York',
-    offset: -5,
-  },
-];
-
-ReactDOM.render(<App localTimeZone={localTimeZone} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

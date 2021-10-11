14aec67b965158b1881da237b552aa833e584baa
Antikiok <antikiok@gmail.com>
2021-10-11 22:54:30 +0300

    1

--- Lesson9/task3/src/App.jsx
+++ Lesson9/task3/src/App.jsx
@@ -1,35 +1,12 @@
 import React from 'react';
 import UserForm from './UserForm.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      visible: true,
-    };
-  }
-
-  toggle = () => {
-    this.setState({
-      visible: !this.state.visible,
-    });
+const App = () => {
+  const createUser = userData => {
+    console.log(userData);
   };
 
-  render() {
-    const localTime = this.props.localTimeZone;
-    return (
-      <>
-        <button className="btn" onClick={this.toggle}>
-          Hide
-        </button>
-        {this.state.visible &&
-          localTime.map(time => (
-            <UserForm key={time.id} location={time.location} offset={time.offset} />
-          ))}
-      </>
-    );
-  }
-}
+  return <UserForm onSubmit={createUser} />;
+};
 
 export default App;

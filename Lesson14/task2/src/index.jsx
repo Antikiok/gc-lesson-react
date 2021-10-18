0a729c5f65646e3842f914df44ef3d3f61d9f7f3
Antikiok <antikiok@gmail.com>
2021-10-18 14:12:58 +0300

    1

---
+++ Lesson14/task2/src/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import './index.scss';
+import User from './User.jsx';
+
+ReactDOM.render(<User userId="facebook" />, document.getElementById('root'));
